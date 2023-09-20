8b7ff4afa745835536a701674ad2677439c90dc1
gehirudm <gehirudm@pm.me>
2023-09-20 22:10:12 +0530

    JS Day 1 part 2

--- 2023/JS/Day 1 - Calorie Counting/index.js
+++ 2023/JS/Day 1 - Calorie Counting/index.js
@@ -1,11 +1,12 @@
 import { readFileSync } from "node:fs";
 const fileContent = readFileSync('./data.txt', { encoding: 'utf-8' });
-const highest = fileContent
+const caloriesSortedArray = fileContent
     .split(/\r?\n\r\n/)
     .map(block => block.split(/\r\n/))
     .map(block => block
     .map(item => +item)
     .reduce((acc, curr) => acc + curr, 0))
-    .sort((a, b) => b - a)
-    .find(() => true);
-console.log(highest);
+    .sort((a, b) => b - a);
+const highest = caloriesSortedArray.find(() => true);
+const sumOfHighest3 = caloriesSortedArray.slice(0, 3).reduce((acc, curr) => acc + curr, 0);
+console.log(highest, sumOfHighest3);
--- 2023/JS/Day 1 - Calorie Counting/index.ts
+++ 2023/JS/Day 1 - Calorie Counting/index.ts
@@ -2,7 +2,7 @@ import { readFileSync } from "node:fs";
 
 const fileContent = readFileSync('./data.txt', { encoding: 'utf-8' });
 
-const highest = fileContent
+const caloriesSortedArray = fileContent
     .split(/\r?\n\r\n/)
     .map(block =>
         block.split(/\r\n/)
@@ -13,6 +13,8 @@ const highest = fileContent
             .reduce((acc, curr) => acc + curr, 0)
     )
     .sort((a, b) => b - a)
-    .find(() => true)
 
-console.log(highest);
+const highest = caloriesSortedArray.find(() => true)
+const sumOfHighest3 = caloriesSortedArray.slice(0, 3).reduce((acc, curr) => acc + curr, 0)
+
+console.log(highest, sumOfHighest3);
