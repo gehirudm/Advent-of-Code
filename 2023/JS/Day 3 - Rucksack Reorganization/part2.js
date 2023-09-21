116ecd014beab1d954ec6962a98b92773e3dc36b
gehirudm <gehirudm@pm.me>
2023-09-21 22:44:03 +0530

    Day 2 JS Part 2

--- 2023/JS/Day 3 - Rucksack Reorganization/part2.js
+++ 2023/JS/Day 3 - Rucksack Reorganization/part2.js
@@ -1,12 +1,15 @@
 import { readFileSync } from "node:fs";
-const split = (str, index) => [str.slice(0, index), str.slice(index)];
 const encoder = new TextEncoder();
 const fileContent = readFileSync('./data.txt', { encoding: 'utf-8' });
 const prioritySum = fileContent
     .split(/\r\n/) // Split by new line
-    .map(line => split(line, line.length / 2)) // Split the line in the middle
+    .reduce(([accHead, ...accTail], curr) => // Destructure the accumulator head and tail
+ accHead.length < 3 ? // Check if the head of the accumulator is filled
+    [[...accHead, curr], ...accTail] : // Not filled, so add to head
+    [[curr], accHead, ...accTail] // Filled, add new item to accumulator
+, [[]]) // Group into sets of 3 lines
     .map(group => group.map(item => encoder.encode(item))) // Convert the strings into UInt8Arrays
-    .map(([first, second]) => first.filter(item => second.includes(item))) // Find Common items between 2 sides
+    .map(([first, second, third]) => first.filter(item => second.includes(item) && third.includes(item))) // Find Common items between 2 sides
     .map(commons => commons.filter((value, index, array) => array.indexOf(value) === index)) // Get only distinct items
     .map(commons => commons.map(item => item > 96 ? item - 96 : item - 38)) // Map items into priority score
     .reduce((acc, curr) => acc + curr.reduce((acc, curr) => acc + curr, 0), 0); // Sum up the priority score
