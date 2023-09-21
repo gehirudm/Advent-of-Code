33db726151f0037cd340f932af5cadc3227b32b6
gehirudm <gehirudm@pm.me>
2023-09-21 22:26:42 +0530

    Day 3 JS Part 1

---
+++ 2023/JS/Day 3 - Rucksack Reorganization/index.ts
@@ -0,0 +1,23 @@
+import { readFileSync } from "node:fs";
+
+const split = (str: string, index: number) => ([str.slice(0, index), str.slice(index)] as const);
+
+const encoder = new TextEncoder();
+
+const fileContent = readFileSync('./data.txt', { encoding: 'utf-8' });
+
+const logAndReturn = <T>(item: T) => {
+    console.log(item);
+    return item;
+}
+
+const prioritySum = fileContent
+    .split(/\r\n/) // Split by new line
+    .map(line => split(line, line.length / 2)) // Split the line in the middle
+    .map(group => group.map(item => encoder.encode(item))) // Convert the strings into UInt8Arrays
+    .map(([first, second]) => first.filter(item => second.includes(item))) // Find Common items between 2 sides
+    .map(commons => commons.filter((value, index, array) => array.indexOf(value) === index)) // Get only distinct items
+    .map(commons => commons.map(item => item > 96 ? item - 96 : item - 38)) // Map items into priority score
+    .reduce((acc, curr) => acc + curr.reduce((acc, curr) => acc + curr, 0), 0) // Sum up the priority score
+
+console.log(prioritySum);
