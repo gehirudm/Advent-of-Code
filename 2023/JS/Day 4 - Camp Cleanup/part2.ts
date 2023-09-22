148c378c5fff669e86009b2df27201cc0da5a89b
gehirudm <gehirudm@pm.me>
2023-09-22 23:15:13 +0530

    JS Day 4 part 2

--- 2023/JS/Day 4 - Camp Cleanup/part2.ts
+++ 2023/JS/Day 4 - Camp Cleanup/part2.ts
@@ -2,8 +2,9 @@ import { readFileSync } from "node:fs";
 
 const fileContent = readFileSync('./data.txt', { encoding: 'utf-8' });
 
-const isContained = ([[L1, L2], [R1, R2]]: readonly [readonly [number, number], readonly [number, number]]) =>
-    (L1 >= R1 && L2 <= R2) || (R1 >= L1 && R2 <= L2)
+const doOverlap = ([[L1, L2], [R1, R2]]: readonly [readonly [number, number], readonly [number, number]]) =>
+    Math.max(L1,R1) <= Math.min(L2,R2)
+
 
 const containedPairCount = fileContent
     .split(/\r\n/) // Split by new line
@@ -11,7 +12,7 @@ const containedPairCount = fileContent
         .map(range => range.split("-"))
         .map(item => [+item[0], +item[1]] as const))
     .map(item => [item[0], item[1]] as const) // Convert the line into an array of tuples of tuple of range [[[2,4], [5,8]], ...]
-    .filter(isContained)
+    .filter(doOverlap)
     .length
 
 console.log(containedPairCount);
