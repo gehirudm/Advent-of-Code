d8b39a9f476c2d9678b81838e6de891864db4324
gehirudm <gehirudm@pm.me>
2023-09-27 18:57:46 +0530

    day 6 JS

---
+++ 2023/JS/Day 6 - Tuning Trouble/part1.js
@@ -0,0 +1,13 @@
+import { readFileSync } from "node:fs";
+const fileContent = readFileSync('./data.txt', { encoding: 'utf-8' });
+const stream = fileContent.trimEnd();
+for (let index = 0; index < stream.length; index++) {
+    const is4LettersUnique = stream
+        .slice(index, index + 4) // Section of 4 Letters
+        .split("") // Split into characters
+        .filter((val, idx, arr) => arr.lastIndexOf(val) != idx).length == 0; // Check if there's repeated characters
+    if (is4LettersUnique) {
+        console.log(index + 4, stream.slice(index, index + 4));
+        break;
+    }
+}
---
+++ 2023/JS/Day 6 - Tuning Trouble/part2.ts
@@ -0,0 +1,17 @@
+import { readFileSync } from "node:fs";
+
+const fileContent = readFileSync('./data.txt', { encoding: 'utf-8' });
+const stream = fileContent.trimEnd();
+
+for (let index = 0; index < (stream.length - 14); index++) {
+    const is4LettersUnique =
+        stream
+            .slice(index, index + 14) // Section of 4 Letters
+            .split("") // Split into characters
+            .filter((val, idx, arr) => arr.lastIndexOf(val) != idx).length == 0 // Check if there's repeated characters
+
+    if (is4LettersUnique) {
+        console.log(index + 14, stream.slice(index, index + 14));
+        break;
+    }
+}
